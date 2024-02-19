b9aaded5b3865d351d2cce5a637090b7946c5fc2
Ruchitha007 <143633364+ruchitha007@users.noreply.github.com>
2024-02-18 19:45:06 -0500

    Add files via upload

---
+++ src/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+
+
+    <div className="center" aria-label="CopyRights Information">
+
+      All rights reserved &copy; @ Fabio Nolasco
+
+
+      <div className="h-card">
+        
+        <p className="p-country-name">USA</p>
+        
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
